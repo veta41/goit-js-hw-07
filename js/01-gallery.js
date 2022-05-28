75f93c2ca99a35a9171791def17bad12e3bc48d4
Svitlana Chudesa <chsveta34@gmail.com>
2022-05-28 14:24:56 +0300

    add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне. 
+
+function ceateCardsMarkup(galleryItems) {
+  return galleryItems
+  .map(({preview, description}) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}}"
+    />
+  </a>
+</div>
+     
+    `
+  })
+  .join('')
+  
+}
