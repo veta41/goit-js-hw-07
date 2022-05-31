2f8a0f5dce4478c177e9e56b46e1d74ef6d04e6d
Svitlana Chudesa <chsveta34@gmail.com>
2022-05-31 21:59:56 +0300

    add task2 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery')
+const cardMarkup = ceateCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkup );
+
+function ceateCardsMarkup(galleryItems) {
+  return galleryItems
+  .map(({preview, description, original}) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+     
+    `
+  })
+  .join('')
+  
+}
